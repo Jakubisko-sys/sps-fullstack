4ef18a6605008c80a2da5ba86a7475684b6a7a46
Jakub Zeman <jakub.zeman1@student.sps-cl.cz>
2025-06-03 21:59:59 +0200

    feat: added schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,23 @@
--- Vytvor tabulku "items"
-CREATE TABLE IF NOT EXISTS items ( 
+-- Vytvor tabulku "visitors"
+CREATE TABLE IF NOT EXISTS visitors ( 
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL, 
-  description TEXT
+  firstname TEXT NOT NULL, 
+  lastname TEXT NOT NULL
+)
+
+--vytvor tabulku "animals"
+CREATE TABLE IF NOT EXISTS animals (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  species TEXT NOT NULL,
+)
+
+--vytvor tabulku "visits"
+CREATE TABLE IF NOT EXISTS visits (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  visitor_id INTEGER NOT NULL,
+  animal_id INTEGER NOT NULL,
+  visit_date DATE NOT NULL,
+  FOREIGN KEY (visitor_id) REFERENCES visitors(id),
+  FOREIGN KEY (animal_id) REFERENCES animals(id)
 )
