2d2cc7d3cbcf7d9ebf6f07b9d869a1742e9561cc
Jakub Zeman <jakub.zeman1@student.sps-cl.cz>
2025-06-07 22:05:37 +0200

    added server.ts

--- server.ts
+++ server.ts
@@ -1,20 +1,23 @@
-const express = require('express');
-const path = require('path');
+import express, { Request, Response } from 'express';
+import path from 'path';
 
 // const { initializeDatabase } = require('./database/db'); // vytváření databáze
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port: number = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const visitorsRoutes = require('./api/visitors');
+import visitorsRoutes from './api/visitors';
 app.use('/api/visitors', visitorsRoutes);
 
-const animalsRoutes = require('./api/animals');
+import animalsRoutes from './api/animals';
 app.use('/api/animals', animalsRoutes);
+
+import visitsRoutes from './api/visits';
+app.use('/api/visits', visitsRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
